44511d0c694e74f5aea73bee0881d189d0d54bd5
rcedric_246 <rcedric.cruz@gmail.com>
2026-05-25 22:09:50 +0800

    Update ACT 1.cpp

--- ACT 1.cpp
+++ ACT 1.cpp
@@ -7,14 +7,4 @@ int main () {
   int positiveCount = 0;
   int negativeCount = 0;
 
-  while (true) 
-  {
-    if (num == 0) {
-      break; {
-        else if (num  > 0) {
-          
-        }
-      }
-    }
-  }
-}
+  
