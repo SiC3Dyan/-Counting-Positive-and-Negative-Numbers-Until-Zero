5f0102d2ee44b556ebcf2a769924c259d13749a2
rcedric_246 <rcedric.cruz@gmail.com>
2026-05-28 10:34:49 +0800

    Update ACT 1.cpp

--- ACT 1.cpp
+++ ACT 1.cpp
@@ -1,27 +1,2 @@
 #include <iostream> 
-using namspace std;
-
-int main () {
-
-  int num;
-  int poositiveCount = 0;
-  int negativeCount = 0;
-
-  while (true) 
-  {
-
-    else if (num == 0) {
-      break;
-    } else if (num > 0) {
-      positiveCount++;
-    } else {
-      negativeCount++;
-    }
-  }
-
-  cout << "Positive Numbers entered." << positiveCount << endl;
-  cout << "Negative Numbers entered." << negativeCount << endl;
-
-
-  return 0;
-}
+using namespace std;
